9f2b97b2841fa18b3201f06dad8d1c8a8769559c
brunolopez312 <brunolopez15@yahoo.com>
2022-12-12 13:06:04 -0800

    fix the classes of job description

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Data Analyst", "Stanford Researcher"],
+        strings: ["Data Scientist", "Stanford Researcher", "Musician", "Geospatial Analyst"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Data Analyst", "Stanford Researcher"],
+        strings: ["Data Scientist", "Stanford Researcher", "Musician", "Geospatial Analyst"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
