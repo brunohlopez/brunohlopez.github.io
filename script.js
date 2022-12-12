4a05b4b3096ad18441657d7506466dc247d5d5c1
brunolopez312 <brunolopez15@yahoo.com>
2022-12-12 13:10:10 -0800

    fixed new text

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Data Scientist", "Stanford Researcher", "Musician", "Geospatial Analyst"],
+        strings: ["Data Scientist", "Former Stanford Researcher", "Musician", "Geospatial Analyst", "1st Gen College Student"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Data Scientist", "Stanford Researcher", "Musician", "Geospatial Analyst"],
+        strings: ["Data Scientist", "Former Stanford Researcher", "Musician", "Geospatial Analyst", "1st Gen College Student"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
